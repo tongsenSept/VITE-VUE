fe62031cb6945d8076f04f906f326986457aa181
tongsen <tongsen@makeblock.com>
2023-08-30 19:44:03 +0800

    chore: 初始化项目

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import nightwatchPlugin from 'vite-plugin-nightwatch'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    minify: false,
+    rollupOptions: {
+      // external: ['vue'],
+      output: {
+        minifyInternalExports: false,
+      }
+    },
+  },
+  plugins: [
+    vue(),
+    vueJsx(),
+    nightwatchPlugin(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+
+})
